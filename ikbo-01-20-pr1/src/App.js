690fb51e166155e2da7d54ebaec56c3396036b06
ArmorDs <zeleznoff84-00@mail.ru>
2023-03-01 11:12:28 +0300

    add pr1

---
+++ ikbo-01-20-pr1/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <div color={"red"}>Make study fun</div>
+            </header>
+            <body>
+            <div style={{color: "red"}}>
+                Zhelenov D.A.
+            </div>
+            </body>
+        </div>
+    );
+}
+
+export default App;
